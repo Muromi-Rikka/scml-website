b6f43216bc09b0943b78b283d94d159890816156
rikka <admin@rikka.cc>
2024-10-14 14:20:08 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,33 +4,33 @@ import tailwind from '@astrojs/tailwind'
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight(
-      {
-        title: 'SugarCube 2 ModLoader',
-        social: {
-          github: 'https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader',
-        },
-        defaultLocale: 'zh-cn',
-        locales: {
-          'zh-cn': {
-            label: '简体中文',
-            lang: 'zh-cn',
-          },
-          'en': {
-            label: 'English',
-          },
-        },
-        sidebar: [
-          {
-            label: 'ModLoader',
-            autogenerate: { directory: 'mod-loader' },
-          },
-        ],
+  site: 'https://modloader.pages.dev',
+  integrations: [starlight(
+    {
+      title: 'SugarCube 2 ModLoader',
+      social: {
+        github: 'https://github.com/Lyoko-Jeremie/sugarcube-2-ModLoader',
       },
-    ),
-    tailwind(),
-  ],
+      defaultLocale: 'zh-cn',
+      locales: {
+        'zh-cn': {
+          label: '简体中文',
+          lang: 'zh-cn',
+        },
+        'en': {
+          label: 'English',
+        },
+      },
+      sidebar: [
+        {
+          label: 'ModLoader',
+          autogenerate: { directory: 'mod-loader' },
+        },
+      ],
+    },
+  ), tailwind(), sitemap()],
 })
