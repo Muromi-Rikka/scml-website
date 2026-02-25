57c2063a3329fa485c081ab81b4b0825385345c8
rikka <admin@rikka.cc>
2026-02-25 12:37:19 +0800

    Add plugins for enhanced functionality in rspress.config.ts
    - Added @rspress/plugin-llms and @rspress/plugin-sitemap as devDependencies in package.json and bun.lock.
    - Integrated the new plugins into rspress.config.ts to enable LLMS and sitemap features.
    - Updated configuration to include additional UI options and edit link for documentation.

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,8 +1,10 @@
 import * as path from "node:path";
 import { defineConfig } from "@rspress/core";
-
+import { pluginLlms } from "@rspress/plugin-llms";
+import { pluginSitemap } from "@rspress/plugin-sitemap";
 export default defineConfig({
   root: path.join(__dirname, "docs"),
+  plugins: [pluginLlms(), pluginSitemap({ siteUrl: "https://modloader.pages.dev" })],
   title: "ModLoader 文档",
   icon: "/logo.png",
   logo: {
@@ -10,6 +12,14 @@ export default defineConfig({
     dark: "/logo.png",
   },
   themeConfig: {
+    llmsUI: true,
+    lastUpdated: true,
+    enableContentAnimation: true,
+    enableAppearanceAnimation: true,
+    enableScrollToTop: true,
+    editLink: {
+      docRepoBaseUrl: "https://github.com/Muromi-Rikka/scml-website/tree/master/docs",
+    },
     socialLinks: [
       {
         icon: "github",
